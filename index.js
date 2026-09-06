c6229b19a266bbefbca10811ae2a2489d5b22c7f
aihaocc <33190240+aihaocc@users.noreply.github.com>
2026-09-06 16:51:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// import { kv } from '@vercel/kv';
-const kv = null; // 禁用 KV，使用内存模式
+import { kv } from '@vercel/kv';
+//const kv = null; // 禁用 KV，使用内存模式
 
 // ========== 配置常量 ==========
 const KV_BATCH_PREFIX = "batch_";
